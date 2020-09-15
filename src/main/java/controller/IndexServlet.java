dbcf6df313b9d995fe389b8c2eba2333cb7b9e6e
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-09-15 11:34:26 -0500

    adding html to index.jsp and created scripts

--- src/main/java/controller/IndexServlet.java
+++ src/main/java/controller/IndexServlet.java
@@ -2,22 +2,30 @@ package controller;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="IndexServlet", urlPatterns="/")
-public class IndexServlet {
+@WebServlet(name = "IndexServlet", urlPatterns = "/home")
+public class IndexServlet extends HttpServlet {
     // We want to display our 'home page' using this servlet,
-        // when someone loads 'http://localhost:8080/'
+    // when someone loads 'http://localhost:8080/'
 
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // this is where we want to tell the Tomcat sever to display the webapp/index.jsp template file (as webpage)
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
 
-        out.println("Welcome to Burgers 'R Us!");
+        // The BORING way
+        // resp.setContentType("text/html");
+        // resp.getWriter().println("<h1>Welcome to Burgers 'R Us!</h1>");
         // END: The BORING way
+
+        // The FUN way!
+        // we want to load the index.jsp template, when this Servlet is called upon
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        // END: The FUN way!
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -11,9 +11,15 @@
 
 <html>
 <head>
-    <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Burgers 'R Us!</title>
 </head>
 <body>
+<div class="container">
+    <h1>Welcome to Burgers 'R US!</h1>
 
+</div>
+
+<jsp:include page="partials/scripts.jsp" />
 </body>
 </html>
